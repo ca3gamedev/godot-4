51f6858c78ce2efec4a74607ab3bf4795b8fb013
ca3gamedev <ca3gamedev@gmail.com>
2023-12-04 03:38:18 -0500

    kaliina mockup GUI

--- Kaliina/Scenes/Battle/Managers/TimerHP.gd
+++ Kaliina/Scenes/Battle/Managers/TimerHP.gd
@@ -5,19 +5,18 @@ extends Node
 @export var timeleft : float
 
 func _ready():
-	%GUI.get_node("TimerHP").max_value = timeleft
-	%GUI.get_node("TimerHP").value = timeleft
+	%GUI.get_node("Timer").text = str(int(timeleft))
 
 
 func _process(delta):
 	
 	if not CombatData.Protected:
 		timeleft -= delta * levels
-		%GUI.get_node("TimerHP").value = timeleft
+		%GUI.get_node("Timer").text = str(int(timeleft))
 	else:
 		var time = (CombatData.score / 500) * delta
 		timeleft -= time
-		%GUI.get_node("TimerHP").value = timeleft
+		%GUI.get_node("Timer").text = str(int(timeleft))
 	
-	if %GUI.get_node("TimerHP").value <= 0 :
+	if timeleft <= 0 :
 		get_tree().change_scene_to_file(GameOver)
--- Kaliina/Scenes/Battle/Variables/DataPaths.gd
+++ Kaliina/Scenes/Battle/Variables/DataPaths.gd
@@ -1,5 +1,5 @@
 extends Node2D
 
-@export var MyGUI : Node2D
+@export var MyGUI : CanvasLayer
 @export var Onager : Node2D
 @export var Rosa : CharacterBody2D
